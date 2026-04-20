f765eda9faa9f267624cf8497fa781fa5dbaf95a
erwin cris <irwen1703@gmail.com>
2026-04-20 18:56:22 +0800

    Capture book form data via FormData on submission

--- js_file.js
+++ js_file.js
@@ -1,3 +1,16 @@
+const form = document.querySelector(".book-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const bookInfo = new FormData(form);
+
+  const title = bookInfo.get("book_title");
+  const author = bookInfo.get("book_author");
+  const pages = bookInfo.get("book_pages");
+  const readStatus = bookInfo.get("read_status") !== null;
+});
+
 const myLibrary = [];
 
 function Book(title, author, pages) {
