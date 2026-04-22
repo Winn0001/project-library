910ba6c9a2a580bb19cee17068f61d999def95bc
erwin cris <irwen1703@gmail.com>
2026-04-22 22:22:47 +0800

    Fix a typo in query selector argument

--- js_file.js
+++ js_file.js
@@ -47,7 +47,7 @@ function displayBook(book) {
   bookCardClone.querySelector(".read-toggle-btn").textContent = book.status
     ? "Read"
     : "Unread";
-  bookCardClone.querySelector("delete-btn").dataset.id = book.id;
+  bookCardClone.querySelector(".delete-btn").dataset.id = book.id;
 
   booksContainer.appendChild(bookCardClone);
 }
