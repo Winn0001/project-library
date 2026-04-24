36f69a00150469edd799a628e59c3c9e4114f5cf
erwin cris <irwen1703@gmail.com>
2026-04-24 17:44:30 +0800

    Capitalize text content of status toggle button

--- js_file.js
+++ js_file.js
@@ -67,7 +67,8 @@ function displayBook(book) {
   bookCardClone.querySelector(".book-title").textContent = book.title;
   bookCardClone.querySelector(".author-name").textContent = book.author;
   bookCardClone.querySelector(".number-of-pages").textContent = book.pages;
-  bookCardClone.querySelector(".status-toggle-btn").textContent = book.status;
+  bookCardClone.querySelector(".status-toggle-btn").textContent =
+    book.status.charAt(0).toUpperCase() + book.status.slice(1);
   bookCardClone.querySelector(".status-toggle-btn").classList.add(book.status);
   bookCardClone.querySelector(".book-card").dataset.id = book.id;
 
